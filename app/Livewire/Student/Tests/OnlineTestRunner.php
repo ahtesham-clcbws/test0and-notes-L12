394649b4335f3e97344ee6bdd1e6c74c948e8603
Ahtesham <ahtesham2000@gmail.com>
2026-03-30 17:03:49 +0530

    [CHORE] updated OnlineTestRunner layout logic and setup backup before transition

--- app/Livewire/Student/Tests/OnlineTestRunner.php
+++ app/Livewire/Student/Tests/OnlineTestRunner.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
-#[Layout('Layouts.exam')]
+#[Layout('components.layouts.student-mary')]
 class OnlineTestRunner extends Component
 {
     public $testId;
